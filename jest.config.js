a3e7304cc04fac3a81843bfda8c5f014844dfe62
Keith Bingman <keith.bingman@unbounce.com>
2019-11-15 11:31:22 -0800

    jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   testMatch: ['test/*.spec.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json'
+    }
   }
 };
